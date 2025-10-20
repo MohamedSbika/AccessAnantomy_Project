ac3e5d243de7e87ee4270054554f0442a24f3eb8
MohamedSbika <sbikamohamed241@gmail.com>
2025-10-20 12:02:26 +0100

    last

---
+++ application/logs/log-2025-10-18.php
@@ -0,0 +1,4 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2025-10-18 22:49:43 --> Severity: Notice --> Undefined variable: listActualites C:\xampp\7.4\htdocs\public_html\application\views\page_home.php 1111
+ERROR - 2025-10-18 22:49:43 --> Severity: Warning --> sizeof(): Parameter must be an array or an object that implements Countable C:\xampp\7.4\htdocs\public_html\application\views\page_home.php 1111
